afd12022b2041cd2a4d4d1bbbe269a21f87e92f3
Andrew Crosio <andrew.crosio@gmail.com>
2013-06-07 14:26:06 +0400

    master: fixing test matcher

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import re
 
 
 COVERAGE_PATH = '.coverage'
-IGNORED_NAME_PORTIONS = ['test', re.compile(r'(?:\b|^)docs/')]
+IGNORED_NAME_PORTIONS = [re.compile(r'(?:^|[_.-])test.py'), re.compile(r'(?:\b|^)docs/')]
 REQUIRED_NAME_PORTIONS = [re.compile(r'\.py$')]
 OUTPUT_COVERAGE_DOC = 'diff_coverage_html'
 COMPARE_WITH_BRANCH = 'master'
