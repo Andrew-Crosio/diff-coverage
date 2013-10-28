1850e11cefea8ccf222d135209c9f050fa96e84c
Andrew Crosio <andrew.crosio@gmail.com>
2013-10-28 21:40:10 +0400

    to_egg: converting to egg

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'publish':
+    # Publish to PyPi.
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+long_description = 'Information and documentation at https://github.com/Andrew-Crosio/diff-coverage'
+
+setup(name='diffcoverage',
+      version='1.0',
+      author='Some guy',
+      author_email='some@guy.com',
+      url='https://github.com/Andrew-Crosio/diff-coverage',
+      license='Unlicense',
+      description='Get a difference in coverage with a patch.',
+      long_description=long_description,
+      packages=find_packages(),
+      include_package_data=True,
+      platforms=['any'],
+      classifiers=[
+          'Topic :: Internet',
+          'Natural Language :: English',
+          'Development Status :: 1.0 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: Freely Distributable',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ],
+      entry_points={
+          'console_scripts': [
+              'diffcoverage = diffcoverage/diff_coverage.py:_main',
+          ],
+      },
+      install_requires=[],
+      tests_require=[],
+)
