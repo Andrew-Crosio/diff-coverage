edc13c2c5eee83c813b117ddc4f1f41b06a20c2f
Andrew Crosio <andrew@andrewcrosio.com>
2016-02-03 21:10:30 +0700

    updating setup py

--- setup.py
+++ setup.py
@@ -1,41 +1,37 @@
-import os
-import sys
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
-if sys.argv[-1] == 'publish':
-    # Publish to PyPi.
-    os.system('python setup.py sdist upload')
-    sys.exit()
 
-long_description = 'Information and documentation at https://github.com/Andrew-Crosio/diff-coverage'
-
-setup(name='diffcoverage',
-      version='1.0',
-      author='Some guy',
-      author_email='some@guy.com',
-      url='https://github.com/Andrew-Crosio/diff-coverage',
-      license='Unlicense',
-      description='Get a difference in coverage with a patch.',
-      long_description=long_description,
-      packages=find_packages(),
-      include_package_data=True,
-      platforms=['any'],
-      classifiers=[
-          'Topic :: Internet',
-          'Natural Language :: English',
-          'Development Status :: 1.0 - Production/Stable',
-          'Intended Audience :: Developers',
-          'License :: Freely Distributable',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
+setup(
+    name='diffcoverage',
+    version='1.1',
+    author='Andrew Crosio',
+    author_email='andrew@andrewcrosio.com',
+    url='https://github.com/gxx/diff-coverage',
+    license='Unlicensed',
+    description='Measure difference in coverage with a difference patch',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    package_data={
+        'diffcoverage': ['templates/*/*.html']
+    },
+    include_package_data=True,
+    platforms=['any'],
+    classifiers=[
+      'Topic :: Internet',
+      'Natural Language :: English',
+      'Intended Audience :: Developers',
+      'License :: Freely Distributable',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
+    ],
+    entry_points={
+      'console_scripts': [
+          'diffcoverage = diffcoverage.diff_coverage:main',
       ],
-      entry_points={
-          'console_scripts': [
-              'diffcoverage = diffcoverage.diff_coverage:main',
-          ],
-      },
-      install_requires=[
-          'coverage==3.7'
-      ],
-      tests_require=[],
+    },
+    install_requires=[
+      'coverage>=3.7'
+    ],
+    tests_require=[],
 )
