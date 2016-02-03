beed5af7ed55d1cce87aa50f6548da487c5aaf0d
Andrew Crosio <andrew@andrewcrosio.com>
2016-02-03 21:10:04 +0700

    removing silly settings file

--- diffcoverage/settings.py
+++ diffcoverage/settings.py
@@ -8,12 +8,5 @@ IGNORED_NAME_PORTIONS = [re.compile(r'(?:^|[_.-])test.py'), re.compile(r'(?:\b|^
 REQUIRED_NAME_PORTIONS = [re.compile(r'\.py$')]
 OUTPUT_COVERAGE_DOC = 'diff_coverage_html'
 COMPARE_WITH_BRANCH = 'master'
-HTML_DIFF_REPORT_PATH = 'diff_coverage.html'
 XML_REPORT_FILE = 'coverage.xml'
 HTML_REPORT_DIR = 'coverage_html'
-
-
-try:
-    execfile('globalsettings')
-except IOError:
-    pass
